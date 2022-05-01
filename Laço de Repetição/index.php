f20df79a46362987bd4b45d2555317d2ec3383a3
Lorena Araujo Almeida <87384833+lorenaaraujoo@users.noreply.github.com>
2022-05-01 15:42:39 -0300

    Update index.php

--- Laço de Repetição/index.php
+++ Laço de Repetição/index.php
@@ -1,7 +1,6 @@
     <?php
 
-    /*
-    //Exercício 1
+    //Exercício 1 - Desenvolva uma página que a partir dos números de uma placa de um veículo informe o dia de rodízio de um veículo.
     $placa = "MNS2757";
     echo($placa[strlen($placa)-1]);
     switch($placa[strlen($placa)-1]){
@@ -30,11 +29,9 @@
             echo(" Seu carro não pode andar sexta-feira!");
             break;
     }
-    */
+    echo("<hr>");
 
-    /*
-    //Exercício 2
-
+    //Exercício 2 - Desenvolva uma página que a partir de três números inteiros e determine qual é o menor e qual é o maior.
     $num1 = 1000;
     $num2 = 20;
     $num3 = 21;
@@ -42,11 +39,15 @@
     $variaveis = array($num1, $num2, $num3);
     echo "O maior número é: ".max($variaveis);
     echo "<br> O menor número é: ".min($variaveis);
-    */
-
+    echo("hr>");
     
-    /*
-    //Exercício 3
+   //Exercício 3 - Criar uma página que leia o salário de um funcionário e o departamento que ele trabalha. Para o departamento Operacional, o aumento será de 15%. Para o departamento de desenvolvimento deverão ser aplicadas as seguintes regras:
+    // $depto = 1;//Operacional
+    // $depto = 2;//Desenvolvimento
+    // · para 1.500,00 <= salarioAtual < 1.750,00: aumento igual a 12%
+    // · para 1.750,00 <= salarioAtual < 2.000,00: aumento igual a 10%
+    // · para 2.000,00 <= salarioAtual < 3.000,00: aumento igual a 7%
+    // · para acima de 3.000,00: aumento igual a 5%.
 
     $salarioAtual = 1800;
     $depto = 2;
@@ -66,12 +67,10 @@
         }
     }
 
-    echo("Aumento de salário:" .$resultado);
-    */
-
-    /*
-    //Exercício 4
-
+     echo("Aumento de salário:" .$resultado);
+     echo("<hr>");
+     
+    //Exercício 4 - Desenvolva um programa para calcular a potência de um número por uma base qualquer. O programa deverá realizar a leitura da base e do expoente.
     $numero = 2;
     $expoente = 2;
     $resultado = $numero;
@@ -80,10 +79,9 @@
         $resultado = $resultado * $numero;
     }
     echo($resultado);
-    */
-
-    /*
-    //Exercício 5
+    echo("<hr>");
+    
+    //Exercício 5 - Desenvolva uma página que ao ler um número diga se ele é primo ou não.
     $num = 5;
     $cont = 0;
     for($i = 1; $i <= $num; $i++){
@@ -96,19 +94,19 @@
     } else {
         echo "O número não é primo!";
     }
-    */
 
-    //Exercício 6
+     echo("<hr>");
+
+    //Exercício 6 - Desenvolva um programa para calcular a soma de todos os números ímpares de 1 à 20 e a multiplicação de todos os números pares de 1 à 20.
+    $multi = 1;
     $soma = 0;
-    $numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-    $multiplicacao = $numeros[0];
-    for ($i = 0; $i < count($numeros); $i++) {
-        if($numeros[$i] % 2 == 0){
-            $multiplicacao = $numeros[$i] * $multiplicacao;
-        }else{
-            $soma = $soma + $numeros[$i];
-        }
-    }
-    echo($soma);
-    echo("<br>".$multiplicacao);
-    ?>
+    for( $i = 1 ; $i <=20; $i++){
+            if ($i % 2  == 1) {
+                $soma += $i;
+            } else if ($i % 2  == 0){
+                $multi *= $i;
+            }
+        }   
+        echo("A multiplicação dos números pares é de: ".$multi. "<br>");
+        echo("A soma dos números ímpares é de: ".$soma);
+    ?>    
